634f23542763db9c86a1297146af2d61fd9a7a09
abdulmuqtadirmalik <abdulmuqtadirmalik77@gmail.com>
2023-03-21 22:32:12 +0500

    Checklist project initial commit

---
+++ Checklist/ChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistApp.swift
+//  Checklist
+//
+//  Created by Abdul Muqtadir Malik on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Checklist/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Checklist
+//
+//  Created by Abdul Muqtadir Malik on 19/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
